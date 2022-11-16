781cc5767ab0506436acb365fe614a21db6caef9
James Addison <71357672+siliconjungle@users.noreply.github.com>
2022-11-16 14:07:45 +1100

    Create min-store.js

---
+++ examples/min-store.js
@@ -0,0 +1,22 @@
+class MinStore {
+  values = {}
+
+  setValue (key, value) {
+    if (this.values[key] === undefined || value < this.values[key]) {
+      this.values[key] = value
+      return true
+    }
+    
+    return false
+  }
+
+  getValue (key) {
+    return this.values[key] ?? null
+  }
+
+  hasValue (key) {
+    return this.values[key] !== undefined
+  }
+}
+
+export default MinStore
